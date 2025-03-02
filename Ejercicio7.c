e1d5324cf9ee10f846a000e0b5f1855e06cfbc26
Cebollino945 <angelhergonzalez0@gmail.com>
2025-03-01 19:10:46 -0600

    Create Ejercicio7.c
    Es la corrección del ejercicio 7 ya que en éste se usó un apuntador.

---
+++ Ejercicio7.c
@@ -0,0 +1,13 @@
+// 7. Accediendo a la Memoria
+#include <stdio.h>
+
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *ptr;
+    ptr = arr;
+    for(int i=0; i<5; i++)
+    printf("la direccion de memoria del arreglo %d es: %p \n",*(ptr+i),*(ptr+i) );
+    // Código para imprimir direcciones de memoria aquí
+    return 0;
+}
